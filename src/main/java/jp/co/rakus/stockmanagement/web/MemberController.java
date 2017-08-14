d2ff406821686c0dc907e378f356e08057056633
shun.nakano <shun.nakano@rakus.co.jp>
2017-08-14 16:14:16 +0900

    (1-5)確認用パスワード欄と、入力ミス時のエラー表示を追加

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
@@ -57,6 +57,10 @@ public class MemberController {
 		if(result.hasErrors()){
 			return form(model);
 		}
+		if(!form.getPassword().equals(form.getRePassword())){
+			model.addAttribute("errorPassword", "異なる値が入力されました。もう一度入力してください");
+			return form(model);
+		}
 		Member checkMailAddressMember = memberService.findByMailAddress(form.getMailAddress());
 		if(checkMailAddressMember != null){
 			model.addAttribute("duplicateMailAddress", "メールアドレスはすでに登録されています");
